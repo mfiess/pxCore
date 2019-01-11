dd4560c9761eeb19e5dfb15b854e71928766d081
mfiess200 <michael_fiess@cable.comcast.com>
2019-01-11 16:59:07 -0500

    checkpoint

--- examples/pxScene2d/src/mac/pxContextUtils.mm
+++ examples/pxScene2d/src/mac/pxContextUtils.mm
@@ -3,46 +3,94 @@
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
 #import <Cocoa/Cocoa.h>
+#include <map>
+#include <thread>
+#include <mutex>
 
-bool glContextIsCurrent = false;
+#include "rtThreadUtils.h"
+
+struct contextData
+{
+    NSOpenGLContext *glContext = nil;
+    NSOpenGLPixelFormat *pixelFormat = nil;
+
+    bool isCurrent = false;
+};
 
 extern NSOpenGLContext *openGLContext;
 
-NSOpenGLContext *internalGLContext = nil;
-NSOpenGLPixelFormat *internalPixelFormat = nil;
+NSOpenGLPixelFormatAttribute attribs[] =
+    {
+        /*NSOpenGLPFADoubleBuffer,*/
+        NSOpenGLPFAAllowOfflineRenderers, // lets OpenGL know this context is offline renderer aware
+        NSOpenGLPFAMultisample, 1,
+        NSOpenGLPFASampleBuffers, 1,
+        NSOpenGLPFASamples, 4,
+        NSOpenGLPFAColorSize, 32,
+        NSOpenGLPFAOpenGLProfile,NSOpenGLProfileVersionLegacy/*, NSOpenGLProfileVersion3_2Core*/, // Core Profile is the future
+        0
+    };
+
+NSOpenGLPixelFormat *emptypixelFormat = [[[NSOpenGLPixelFormat alloc] initWithAttributes:attribs] retain];
+NSOpenGLContext *emptyGlContext = [[NSOpenGLContext alloc] initWithFormat:emptypixelFormat shareContext:nil];
+
+std::recursive_mutex contextsMutex;
+
+std::map<rtThreadId, contextData> backgroundContexts;
 
 
 pxError createGLContext()
 {
-    if (internalGLContext == nil)
+    rtThreadId currentThreadId = rtThreadGetCurrentId();
     {
-        NSOpenGLPixelFormatAttribute attribs[] =
+        std::unique_lock<std::recursive_mutex> {contextsMutex};
+        if ( backgroundContexts.find(currentThreadId) != backgroundContexts.end())
         {
-            /*NSOpenGLPFADoubleBuffer,*/
-            NSOpenGLPFAAllowOfflineRenderers, // lets OpenGL know this context is offline renderer aware
-            NSOpenGLPFAMultisample, 1,
-            NSOpenGLPFASampleBuffers, 1,
-            NSOpenGLPFASamples, 4,
-            NSOpenGLPFAColorSize, 32,
-            NSOpenGLPFAOpenGLProfile,NSOpenGLProfileVersionLegacy/*, NSOpenGLProfileVersion3_2Core*/, // Core Profile is the future
-            0
-        };
-        
-        NSOpenGLPixelFormat *pf = [[NSOpenGLPixelFormat alloc] initWithAttributes:attribs];
-        internalPixelFormat   = [pf retain];
-        internalGLContext = [[NSOpenGLContext alloc] initWithFormat:internalPixelFormat shareContext:openGLContext];
+            return PX_FAIL;
+        }
     }
+    contextData data;
+
+
+
+    NSOpenGLPixelFormat *pf = [[NSOpenGLPixelFormat alloc] initWithAttributes:attribs];
+    data.pixelFormat   = [pf retain];
+    data.glContext = [[NSOpenGLContext alloc] initWithFormat:data.pixelFormat shareContext:openGLContext];
+
+    {
+        std::unique_lock<std::recursive_mutex> {contextsMutex};
+        backgroundContexts[currentThreadId] = data;
+    }
+
     return PX_OK;
 }
 
 pxError makeInternalGLContextCurrent(bool current)
 {
+    rtThreadId currentThreadId = rtThreadGetCurrentId();
+    contextData data;
     if (current)
     {
-        if (internalGLContext == nil)
+        bool contextExists = false;
+        {
+            std::unique_lock<std::recursive_mutex> {contextsMutex};
+            if ( backgroundContexts.find(currentThreadId) != backgroundContexts.end())
+            {
+                contextExists = true;
+                data = backgroundContexts[currentThreadId];
+            }
+        }
+        if (!contextExists)
         {
             createGLContext();
-            [internalGLContext makeCurrentContext];
+            {
+                std::unique_lock<std::recursive_mutex> {contextsMutex};
+                if ( backgroundContexts.find(currentThreadId) != backgroundContexts.end())
+                {
+                    data = backgroundContexts[currentThreadId];
+                }
+            }
+            [data.glContext makeCurrentContext];
             
             glEnable(GL_BLEND);
             glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
@@ -51,26 +99,43 @@ pxError makeInternalGLContextCurrent(bool current)
         }
         else
         {
-            //TODO
+            [data.glContext makeCurrentContext];
         }
-        glContextIsCurrent = true;
+        data.isCurrent = true;
+        {
+            std::unique_lock<std::recursive_mutex> {contextsMutex};
+            backgroundContexts[currentThreadId] = data;
+        }
     }
     else
     {
         [openGLContext makeCurrentContext];
-        glContextIsCurrent = false;
+        std::unique_lock<std::recursive_mutex> {contextsMutex};
+        if ( backgroundContexts.find(currentThreadId) != backgroundContexts.end())
+        {
+            data = backgroundContexts[currentThreadId];
+            data.isCurrent = false;
+            backgroundContexts[currentThreadId] = data;
+        }
     }
     return PX_OK;
 }
 
 pxError requestContextOwnership()
 {
-  [openGLContext makeCurrentContext];
+  if (!rtIsMainThread())
+  {
+    makeInternalGLContextCurrent(true);
+  }
+  //[openGLContext makeCurrentContext];
   return PX_OK;
 }
 
 pxError releaseContextOwnership()
 {
-  [openGLContext clearCurrentContext];
+  if (!rtIsMainThread())
+  {
+    makeInternalGLContextCurrent(false);
+  }
   return PX_OK;
 }
